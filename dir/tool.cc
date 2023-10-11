bedd96da9d73e76a694806217bea732f41ef402b
bochencwx <bochen_cwx@163.com>
2023-10-11 16:43:34 +0800

    add bazel test code

---
+++ dir/tool.cc
@@ -0,0 +1,9 @@
+#include "dir/tool.h"
+
+int Add(int a, int b) {
+    return a + b;
+}
+
+int Sub(int a, int b) {
+    return a - b;
+}
---
+++ dir/tool_test.cc
@@ -0,0 +1,7 @@
+#include "dir/tool.h"
+
+#include "gtest/gtest.h"
+
+TEST(Tool, Add) {
+    ASSERT_EQ(2, Add(1, 1));
+}
