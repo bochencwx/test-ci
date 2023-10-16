9df133d34bc728980079246848a372139e6e10f1
bochencwx <bochen_cwx@163.com>
2023-10-16 15:06:03 +0800

    use docker image for run workflow

--- dir/tool.h
+++ dir/tool.h
@@ -1,5 +1,27 @@
+//
+//
+// Tencent is pleased to support the open source community by making tRPC available.
+//
+// Copyright (C) 2023 THL A29 Limited, a Tencent company.
+// All rights reserved.
+//
+// If you have downloaded a copy of the tRPC source code from Tencent,
+// please note that tRPC source code is licensed under the  Apache 2.0 License,
+// A copy of the Apache 2.0 License is included in this file.
+//
+//
+
 #pragma once
 
 int Add(int a, int b);
 
 int Sub(int a, int b);
+
+namespace tool {
+
+class A {
+public:
+A() {}
+};
+
+}  // namespace tool
--- dir/tool_test.cc
+++ dir/tool_test.cc
@@ -1,8 +1,21 @@
+//
+//
+// Tencent is pleased to support the open source community by making tRPC available.
+//
+// Copyright (C) 2023 THL A29 Limited, a Tencent company.
+// All rights reserved.
+//
+// If you have downloaded a copy of the tRPC source code from Tencent,
+// please note that tRPC source code is licensed under the  Apache 2.0 License,
+// A copy of the Apache 2.0 License is included in this file.
+//
+//
+
 #include "dir/tool.h"
 
 #include "gtest/gtest.h"
 
 TEST(Tool, Add) {
     ASSERT_EQ(2, Add(1, 1));
-    ASSERT_EQ(1, Sub(2, 1));
+    // ASSERT_EQ(1, Sub(2, 1));
 }
